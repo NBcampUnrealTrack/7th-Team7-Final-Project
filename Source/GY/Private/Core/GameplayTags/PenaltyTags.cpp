dc5e1fa40579bb0b27cd0ee6a5f367e0d4f6f827
dooyeon kang <dyhouse0517@gmail.com>
2026-05-11 12:45:54 +0900

    feat: 페널티 GameplayTag 및 인벤토리 EventType enum 등록

---
+++ Source/GY/Private/Core/GameplayTags/PenaltyTags.cpp
@@ -0,0 +1,7 @@
+#include "Core/GameplayTags/PenaltyTags.h"
+
+namespace GYGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(Penalty_Mastery_Insufficient, "Penalty.Mastery.Insufficient");
+	UE_DEFINE_GAMEPLAY_TAG(Penalty_Legendary_Drawback,   "Penalty.Legendary.Drawback");
+}
