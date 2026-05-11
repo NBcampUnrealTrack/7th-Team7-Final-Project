c427f2294189e5aa51860ac851f9b0fe99e6d424
dooyeon kang <dyhouse0517@gmail.com>
2026-05-11 12:44:17 +0900

    feat: SetByCaller 스탯 모디파이어 GameplayTag 등록

---
+++ Source/GY/Public/Core/GameplayTags/OptionTags.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GYGameplayTags
+{
+	// Stat Modifier (SetByCaller 키)
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Stat_Modifier_Deviation)              // ±5% 편차 주입 키
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Stat_Modifier_OptionMagnitude1)
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Stat_Modifier_OptionMagnitude2)
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Stat_Modifier_OptionMagnitude3)
+}
