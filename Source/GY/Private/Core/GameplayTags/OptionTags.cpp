e4fb879d4bf2c51cf8617a1820bf69f1dfe6f69c
dooyeon kang <dyhouse0517@gmail.com>
2026-05-12 22:04:40 +0900

    refactor: GameplayTag 등록 코드 컬럼 정렬 제거

--- Source/GY/Private/Core/GameplayTags/OptionTags.cpp
+++ Source/GY/Private/Core/GameplayTags/OptionTags.cpp
@@ -2,8 +2,8 @@
 
 namespace GYGameplayTags
 {
-	UE_DEFINE_GAMEPLAY_TAG(Stat_Modifier_Deviation,         "Stat.Modifier.Deviation");
-	UE_DEFINE_GAMEPLAY_TAG(Stat_Modifier_OptionMagnitude1,  "Stat.Modifier.OptionMagnitude1");
-	UE_DEFINE_GAMEPLAY_TAG(Stat_Modifier_OptionMagnitude2,  "Stat.Modifier.OptionMagnitude2");
-	UE_DEFINE_GAMEPLAY_TAG(Stat_Modifier_OptionMagnitude3,  "Stat.Modifier.OptionMagnitude3");
+	UE_DEFINE_GAMEPLAY_TAG(Stat_Modifier_Deviation, "Stat.Modifier.Deviation");
+	UE_DEFINE_GAMEPLAY_TAG(Stat_Modifier_OptionMagnitude1, "Stat.Modifier.OptionMagnitude1");
+	UE_DEFINE_GAMEPLAY_TAG(Stat_Modifier_OptionMagnitude2, "Stat.Modifier.OptionMagnitude2");
+	UE_DEFINE_GAMEPLAY_TAG(Stat_Modifier_OptionMagnitude3, "Stat.Modifier.OptionMagnitude3");
 }
--- Source/GY/Private/Core/GameplayTags/PenaltyTags.cpp
+++ Source/GY/Private/Core/GameplayTags/PenaltyTags.cpp
@@ -3,5 +3,5 @@
 namespace GYGameplayTags
 {
 	UE_DEFINE_GAMEPLAY_TAG(Penalty_Mastery_Insufficient, "Penalty.Mastery.Insufficient");
-	UE_DEFINE_GAMEPLAY_TAG(Penalty_Legendary_Drawback,   "Penalty.Legendary.Drawback");
+	UE_DEFINE_GAMEPLAY_TAG(Penalty_Legendary_Drawback, "Penalty.Legendary.Drawback");
 }
