0d67927e5acf5edf8b0db86d9d4fbe1837d25bb4
dooyeon kang <dyhouse0517@gmail.com>
2026-05-13 21:45:04 +0900

    feat: LootBox 디버그 cheat 명령 추가

--- Source/GY/Public/Cheats/GYCheatManager.h
+++ Source/GY/Public/Cheats/GYCheatManager.h
@@ -5,6 +5,8 @@
 #include "GameplayTagContainer.h"
 #include "GYCheatManager.generated.h"
 
+class AActor;
+
 UCLASS()
 class GY_API UGYCheatManager : public UCheatManager
 {
@@ -26,6 +28,24 @@ public:
 	UFUNCTION(Exec)
 	void GY_PrintLoadout();
 
+	UFUNCTION(Exec)
+	void GY_SpawnLootBox(const FString& SourceId, const FString& LootTablePath);
+
+	UFUNCTION(Exec)
+	void GY_GetNearestInteractionOptions();
+
+	UFUNCTION(Exec)
+	void GY_InvokeInteraction(const FString& OptionTagName);
+
+	UFUNCTION(Exec)
+	void GY_PrintLootBoxContents();
+
+	UFUNCTION(Exec)
+	void GY_TakeFromLootBox(int32 DropIndex);
+
+	UFUNCTION(Exec)
+	void GY_TakeAllLoot();
+
 private:
 	UFUNCTION(Server, Reliable)
 	void Server_AddItem(const FString& ItemPath, int32 Count);
@@ -35,4 +55,16 @@ private:
 
 	UFUNCTION(Server, Reliable)
 	void Server_UnequipSlot(FGameplayTag SlotTag);
+
+	UFUNCTION(Server, Reliable)
+	void Server_SpawnLootBox(const FString& SourceId, const FString& LootTablePath);
+
+	UFUNCTION(Server, Reliable)
+	void Server_InvokeInteraction(AActor* Target, FGameplayTag OptionTag);
+
+	UFUNCTION(Server, Reliable)
+	void Server_TakeFromLootBox(AActor* Box, int32 DropIndex);
+
+	UFUNCTION(Server, Reliable)
+	void Server_TakeAllLoot(AActor* Box);
 };
