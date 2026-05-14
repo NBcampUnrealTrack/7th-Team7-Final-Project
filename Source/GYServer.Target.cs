d8968bbe63d30ca81cf0e2881ea1b3c64ca41d16
dooyeon kang <dyhouse0517@gmail.com>
2026-05-14 11:09:58 +0900

    chore: UE version 5.7로 변경

--- Source/GYServer.Target.cs
+++ Source/GYServer.Target.cs
@@ -6,7 +6,8 @@ public class GYServerTarget : TargetRules
 	public GYServerTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Server;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 
 		ExtraModuleNames.AddRange( new string[] { "GY" } );
 	}
