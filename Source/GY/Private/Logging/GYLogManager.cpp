e20cee603ce4784eb193ef913a4ecc624a2800e1
yoonseo <cysyes0403@naver.com>
2026-05-15 14:34:08 +0900

    feat: 로그 매니저

---
+++ Source/GY/Private/Logging/GYLogManager.cpp
@@ -0,0 +1,100 @@
+﻿#include "Logging/GYLogManager.h"
+#include "HAL/IConsoleManager.h"
+
+// =============================
+// Categories
+// =============================
+
+DEFINE_LOG_CATEGORY(LogGYContent);
+DEFINE_LOG_CATEGORY(LogGYCombat);
+DEFINE_LOG_CATEGORY(LogGYAI);
+DEFINE_LOG_CATEGORY(LogGYUI);
+DEFINE_LOG_CATEGORY(LogGYPlayer);
+DEFINE_LOG_CATEGORY(LogGYNetwork);
+DEFINE_LOG_CATEGORY(LogGYGame);
+
+// =============================
+// Console Variables
+// =============================
+
+static TAutoConsoleVariable<int32> CVarGYLog(
+	TEXT("gy.log"),
+	1,
+	TEXT("Enable/Disable all GY logs")
+);
+
+static TAutoConsoleVariable<int32> CVarGYLog_KSH(
+	TEXT("gy.log.KSH"),
+	1,
+	TEXT("Enable/Disable KSH logs")
+);
+
+static TAutoConsoleVariable<int32> CVarGYLog_KHB(
+	TEXT("gy.log.KHB"),
+	1,
+	TEXT("Enable/Disable KHB logs")
+);
+
+static TAutoConsoleVariable<int32> CVarGYLog_KDY(
+	TEXT("gy.log.KDY"),
+	1,
+	TEXT("Enable/Disable KDY logs")
+);
+
+static TAutoConsoleVariable<int32> CVarGYLog_LGS(
+	TEXT("gy.log.LGS"),
+	1,
+	TEXT("Enable/Disable LGS logs")
+);
+
+static TAutoConsoleVariable<int32> CVarGYLog_JCM(
+	TEXT("gy.log.JCM"),
+	1,
+	TEXT("Enable/Disable JCM logs")
+);
+
+static TAutoConsoleVariable<int32> CVarGYLog_CYS(
+	TEXT("gy.log.CYS"),
+	1,
+	TEXT("Enable/Disable CYS logs")
+);
+
+static TAutoConsoleVariable<int32> CVarGYLog_KH(
+	TEXT("gy.log.KH"),
+	1,
+	TEXT("Enable/Disable KH logs")
+);
+
+static TAutoConsoleVariable<int32> CVarGYLog_KES(
+	TEXT("gy.log.KES"),
+	1,
+	TEXT("Enable/Disable KES logs")
+);
+
+// =============================
+// Master Toggle
+// =============================
+
+bool FGYLogConfig::IsAllEnabled()
+{
+	return CVarGYLog.GetValueOnGameThread() != 0;
+}
+
+// =============================
+// Developer Toggle
+// =============================
+
+bool FGYLogConfig::IsDeveloperEnabled(const FString& DeveloperName)
+{
+	IConsoleVariable* Var =
+		IConsoleManager::Get().FindConsoleVariable(
+			*FString::Printf(TEXT("gy.log.%s"), *DeveloperName)
+		);
+
+	if (!Var)
+	{
+		return true;
+	}
+
+	return Var->GetInt() != 0;
+}
---
+++ Source/GY/Public/Logging/GYLogManager.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+// =============================
+// Categories
+// =============================
+
+DECLARE_LOG_CATEGORY_EXTERN(LogGYContent, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogGYCombat, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogGYAI, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogGYUI, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogGYPlayer, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogGYNetwork, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogGYGame, Log, All);
+
+// =============================
+// Config
+// =============================
+
+class GY_API FGYLogConfig
+{
+public:
+
+	static bool IsAllEnabled();
+
+	static bool IsDeveloperEnabled(const FString& DeveloperName);
+};
+
+// =============================
+// Macros
+// =============================
+
+#if !UE_BUILD_SHIPPING
+
+#define GY_INTERNAL_LOG(Category, Developer, Verbosity, Format, ...) \
+if (FGYLogConfig::IsAllEnabled() && \
+FGYLogConfig::IsDeveloperEnabled(TEXT(#Developer))) \
+{ \
+UE_LOG(LogGY##Category, Verbosity, TEXT("[%s] ") TEXT(Format), \
+TEXT(#Developer), ##__VA_ARGS__); \
+}
+
+#define GY_LOG(Category, Developer, Format, ...) \
+GY_INTERNAL_LOG(Category, Developer, Log, Format, ##__VA_ARGS__)
+
+#define GY_WARN(Category, Developer, Format, ...) \
+GY_INTERNAL_LOG(Category, Developer, Warning, Format, ##__VA_ARGS__)
+
+#define GY_ERROR(Category, Developer, Format, ...) \
+GY_INTERNAL_LOG(Category, Developer, Error, Format, ##__VA_ARGS__)
+
+#else
+
+#define GY_LOG(Category, Developer, Format, ...)
+#define GY_WARN(Category, Developer, Format, ...)
+#define GY_ERROR(Category, Developer, Format, ...)
+
+#endif
