9c2ac7ecd885203d883e98d0e6e98f5d88302f43
yoonseo <cysyes0403@naver.com>
2026-05-15 18:04:56 +0900

    fix: 퀘스트 데이터 구조 수정

--- Source/GY/Public/Quest/QuestTypes.h
+++ Source/GY/Public/Quest/QuestTypes.h
@@ -14,14 +14,48 @@ enum class EQuestState : uint8
 	Failed
 };
 
+// 퀘스트 이벤트 데이터
+// QuestManager가 받는 공통 이벤트 구조
+USTRUCT(BlueprintType)
+struct FQuestEventData
+{
+	GENERATED_BODY()
+
+	// 이벤트 종류
+	// 예:
+	// Quest.Trigger.AreaEnter
+	// Quest.Objective.Kill
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayTag EventTag;
+
+	// 이벤트 대상
+	// 예:
+	// Zone1
+	// Goblin
+	// Blacksmith
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName TargetId = NAME_None;
+
+	// 진행 수량
+	// 예:
+	// 아이템 여러 개 획득
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Count = 1;
+};
+
 // 퀘스트 목표 하나
 USTRUCT(BlueprintType)
 struct FQuestObjective
 {
 	GENERATED_BODY()
 
+	// 목표 이벤트 종류
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	FGameplayTag EventTag;
+	FGameplayTag ObjectiveEventTag;
+
+	// 대상
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FName TargetId = NAME_None;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	FText Description;
@@ -40,7 +74,7 @@ struct FQuestReward
 	int32 Experience = 0;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	TArray<TSubclassOf<class UObject>> ItemClasses;
+	TArray<TSubclassOf<class UObject>> ItemClasses; // TODO: 아이템 베이스 나오면 수정
 };
 
 // DataTable Row: 에디터에서 퀘스트 데이터를 관리하는 단위
@@ -54,6 +88,14 @@ struct FQuestTableRow : public FTableRowBase
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Quest")
 	FText Description;
+
+	// 활성화 태그
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Quest")
+	FGameplayTag ActivationEventTag;
+
+	// 활성화 태그 대상
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Quest")
+	FName ActivationTargetId = NAME_None;
 
 	// 선행 퀘스트 ID 목록 (DataTable Row Name 기준)
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Quest")
