1de03bf5cd63e86a56f62eb600fa781ba82bf50b
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-05-15 19:40:00 +0900

    feat : 전투 관련 Tags 추가

---
+++ Source/GY/Private/Core/GameplayTags/StateTags.cpp
@@ -0,0 +1,18 @@
+#include "Core/GameplayTags/StateTags.h"
+
+namespace GYStateTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(State_Life_Alive, "State.Life.Alive");
+	UE_DEFINE_GAMEPLAY_TAG(State_Life_Downed, "State.Life.Downed");
+	UE_DEFINE_GAMEPLAY_TAG(State_Life_BeingRevived, "State.Life.BeingRevived");
+	UE_DEFINE_GAMEPLAY_TAG(State_Life_Dead, "State.Life.Dead");
+
+	UE_DEFINE_GAMEPLAY_TAG(State_Hit_Stagger, "State.Hit.Stagger");
+	UE_DEFINE_GAMEPLAY_TAG(State_Hit_KnockDown, "State.Hit.KnockDown");
+	UE_DEFINE_GAMEPLAY_TAG(State_Hit_Stun, "State.Hit.Stun");
+
+	UE_DEFINE_GAMEPLAY_TAG(State_Combat_InCombat, "State.Combat.InCombat");
+	UE_DEFINE_GAMEPLAY_TAG(State_Combat_SuperArmor, "State.Combat.SuperArmor");
+
+	UE_DEFINE_GAMEPLAY_TAG(Action_Reviving, "Action.Reviving");
+}
