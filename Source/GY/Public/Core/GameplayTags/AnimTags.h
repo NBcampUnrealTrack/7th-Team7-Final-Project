7847a0224bd212e9f71277b63ff8e3f366bf820a
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-05-18 17:37:44 +0900

    feat : Animation Montage 재생 용도의 Tag 추가

---
+++ Source/GY/Public/Core/GameplayTags/AnimTags.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace AnimTags
+{
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Anim_Stagger)
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Anim_LookAround)
+};
