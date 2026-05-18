019bc14de317cbf0d2bc51e1ea351c1bff05f83f
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-05-18 15:53:53 +0900

    feat : GMS 메세지 채널 네이티브 태그 정의

---
+++ Source/GY/Private/Core/GameplayTags/GYGameplayMessageTags.cpp
@@ -0,0 +1,35 @@
+﻿#include "Core/GameplayTags/GYGameplayMessageTags.h"
+
+namespace GYGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(Message_Combat_DamageDealt, "GY.Message.Combat.DamageDealt");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Combat_DamageTaken, "GY.Message.Combat.DamageTaken");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Combat_Crit, "GY.Message.Combat.Crit");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Combat_ParrySuccess, "GY.Message.Combat.ParrySuccess");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Combat_BlockSuccess, "GY.Message.Combat.BlockSuccess");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Combat_KnockDown, "GY.Message.Combat.KnockDown");
+
+    UE_DEFINE_GAMEPLAY_TAG(Message_Player_Died, "GY.Message.Player.Died");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Player_Revived, "GY.Message.Player.Revived");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Player_Respawned, "GY.Message.Player.Respawned");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Player_LevelUp, "GY.Message.Player.LevelUp");
+
+    UE_DEFINE_GAMEPLAY_TAG(Message_Inventory_ItemAdded, "GY.Message.Inventory.ItemAdded");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Inventory_ItemRemoved, "GY.Message.Inventory.ItemRemoved");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Inventory_ItemEquipped, "GY.Message.Inventory.ItemEquipped");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Inventory_PotionUsed, "GY.Message.Inventory.PotionUsed");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Inventory_PotionRecharged, "GY.Message.Inventory.PotionRecharged");
+
+    UE_DEFINE_GAMEPLAY_TAG(Message_Build_NodeUnlocked, "GY.Message.Build.NodeUnlocked");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Build_ActionToggled, "GY.Message.Build.ActionToggled");
+
+    UE_DEFINE_GAMEPLAY_TAG(Message_Quest_Started, "GY.Message.Quest.Started");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Quest_Progressed, "GY.Message.Quest.Progressed");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Quest_Completed, "GY.Message.Quest.Completed");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Region_Entered, "GY.Message.Region.Entered");
+
+    UE_DEFINE_GAMEPLAY_TAG(Message_Party_MemberJoined, "GY.Message.Party.MemberJoined");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Party_MemberLeft, "GY.Message.Party.MemberLeft");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Party_MemberDied, "GY.Message.Party.MemberDied");
+    UE_DEFINE_GAMEPLAY_TAG(Message_Chat_Received, "GY.Message.Chat.Received");
+}
---
+++ Source/GY/Public/Core/GameplayTags/GYGameplayMessageTags.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GYGameplayTags
+{
+	// 전투 관련
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Combat_DamageDealt);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Combat_DamageTaken);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Combat_Crit);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Combat_ParrySuccess);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Combat_BlockSuccess);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Combat_KnockDown);
+
+	// 플레이어 생명주기
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Player_Died);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Player_Revived);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Player_Respawned);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Player_LevelUp);
+
+	// 인벤토리, 아이템 - 임시
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Inventory_ItemAdded);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Inventory_ItemRemoved);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Inventory_ItemEquipped);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Inventory_PotionUsed);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Inventory_PotionRecharged);
+
+	// 무기 숙련도 빌드, 노드 관련 - 임시
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Build_NodeUnlocked);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Build_ActionToggled);
+
+	// 퀘스트, 지역 - 임시
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Quest_Started);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Quest_Progressed);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Quest_Completed);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Region_Entered);
+
+	// 멀티 관련 - 친구, 채팅
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Party_MemberJoined);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Party_MemberLeft);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Party_MemberDied);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Chat_Received);
+}
