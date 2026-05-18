f03a0f222b80626f904013c0a3443c28be8a97c4
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-05-18 15:54:37 +0900

    feat : UI 레이어 태그 정의

---
+++ Source/GYUI/Public/GameplayTags/GYUILayerTags.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GYUILayerTags
+{
+	GYUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(UI_Layer_Game);      // HUD 상시 표시
+	GYUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(UI_Layer_Menu);      // 풀스크린 메뉴 - 인벤토리, 무기 빌드, 옵션
+	GYUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(UI_Layer_Modal);     // 최상위 확인창
+}
