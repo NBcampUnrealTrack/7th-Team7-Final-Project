25282426faa92a80e941d61b9d7731edd0b018e9
김성훈 <saltlake5470@gmail.com>
2026-05-19 01:12:51 +0900

    feat: 카메라 오클루전 마스킹 컴포넌트 추가

---
+++ Source/GY/Private/Character/CameraOcclusionMask/CameraOcclusionComponent.cpp
@@ -0,0 +1,107 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/CameraOcclusionMask/CameraOcclusionComponent.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+
+UCameraOcclusionComponent::UCameraOcclusionComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void UCameraOcclusionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ACharacter* Character = Cast<ACharacter>(GetOwner());
+	if (Character == nullptr)
+	{
+		return;
+	}
+
+	SkeletalMeshComp = Character->GetMesh();
+	CameraComp = Character->FindComponentByClass<UCameraComponent>();
+	CapsuleComp = Character->GetCapsuleComponent();
+}
+
+
+void UCameraOcclusionComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                              FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	CameraOcclusionTrace();
+}
+
+void UCameraOcclusionComponent::CameraOcclusionTrace()
+{
+	if (SkeletalMeshComp == nullptr ||
+		CameraComp == nullptr ||
+		CapsuleComp == nullptr)
+	{
+		return;
+	}
+
+	FVector Start = CameraComp->GetComponentLocation();
+	FVector End = SkeletalMeshComp->GetComponentLocation() + MeshOffset;
+
+	FHitResult OutHit;
+	TArray<AActor*> ActorsToIgnore;
+
+	// 시작-끝 사이에 구체 트레이스 투사
+	UKismetSystemLibrary::SphereTraceSingle(this, Start, End,
+	                                        TraceRadius, TraceChannel, false,
+	                                        ActorsToIgnore, DrawDebugType, OutHit, true);
+
+	UPrimitiveComponent* HitComp = OutHit.GetComponent();
+
+	// 히트객체가 없어지면 -  숨긴 객체 복구
+	if (HitComp == nullptr || HitComp == CapsuleComp)
+	{
+		for (TObjectPtr<UPrimitiveComponent>& Wall : HiddenWalls)
+		{
+			if (Wall == nullptr)
+			{
+				continue;
+			}
+			Wall->SetVisibility(true);
+		}
+		HiddenWalls.Empty();
+		return;
+	}
+
+	// Hitcomp 존재 여부 bool 체크, 없으면 추가
+	if (HiddenWalls.Contains(HitComp) == false)
+	{
+		HiddenWalls.Add(HitComp);
+	}
+
+	// 배열 순회후 null 이면 제거, 히트시 숨김.
+	for (int32 i = HiddenWalls.Num() - 1; i >= 0; --i)
+	{
+		UPrimitiveComponent* Wall = HiddenWalls[i];
+
+		if (Wall == nullptr)
+		{
+			HiddenWalls.RemoveAt(i);
+			continue;
+		}
+
+		if (Wall == HitComp)
+		{
+			Wall->SetVisibility(false);
+		}
+		else
+		{
+			Wall->SetVisibility(true);
+			HiddenWalls.RemoveAt(i);
+		}
+	}
+}
---
+++ Source/GY/Public/Character/CameraOcclusionMask/CameraOcclusionComponent.h
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "CameraOcclusionComponent.generated.h"
+
+class UCapsuleComponent;
+class UCameraComponent;
+class USkeletalMeshComponent;
+class UPrimitiveComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GY_API UCameraOcclusionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UCameraOcclusionComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	void CameraOcclusionTrace();
+
+
+private:
+	UPROPERTY()
+	TArray<TObjectPtr<UPrimitiveComponent>> HiddenWalls;
+
+	UPROPERTY()
+	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComp;
+
+	UPROPERTY()
+	TObjectPtr<UCameraComponent> CameraComp;
+
+	UPROPERTY()
+	TObjectPtr<UCapsuleComponent> CapsuleComp;
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Trace)
+	FVector MeshOffset = FVector(0,0,90);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Trace)
+	float TraceRadius = 30.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Trace)
+	TEnumAsByte<ETraceTypeQuery> TraceChannel =  ETraceTypeQuery::TraceTypeQuery1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Trace)
+	TEnumAsByte<EDrawDebugTrace::Type> DrawDebugType = EDrawDebugTrace::ForDuration;
+
+};
