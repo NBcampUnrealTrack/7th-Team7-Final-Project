a1f7f7cccdbf21e9ab6612d8eed2b39ee9d11c23
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-05-18 20:27:54 +0900

    feat : Ability Trigger 감지 Task 구현

---
+++ Source/GY/Public/Enemy/AI/Tasks/BTTask_TriggerAbilityEvent.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_TriggerAbilityEvent.generated.h"
+
+class UGameplayAbility;
+
+UCLASS()
+class GY_API UBTTask_TriggerAbilityEvent : public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+	UBTTask_TriggerAbilityEvent();
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory) override;
+
+	virtual void OnTaskFinished(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory,
+		EBTNodeResult::Type TaskResult) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "GAS", meta = (Categories = "Anim"))
+	FGameplayTag EventTag;
+
+private:
+	void OnAbilityEnded(UGameplayAbility* Ability);
+	void OnAbilityActivated(UGameplayAbility* Ability);
+
+	UPROPERTY()
+	TObjectPtr<UBehaviorTreeComponent> CachedOwnerComp;
+
+	UPROPERTY()
+	TObjectPtr<UGameplayAbility> ActiveAbility;
+};
