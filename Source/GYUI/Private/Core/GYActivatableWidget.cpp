822e7c4337e4a297f4aa21edb0d827e67ce0a6f9
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-05-18 15:55:41 +0900

    feat: GYActivatableWidget 메뉴, 모달 베이스 추가

---
+++ Source/GYUI/Private/Core/GYActivatableWidget.cpp
@@ -0,0 +1,21 @@
+﻿#include "Core/GYActivatableWidget.h"
+#include "CommonInputTypeEnum.h"
+
+UGYActivatableWidget::UGYActivatableWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+TOptional<FUIInputConfig> UGYActivatableWidget::GetDesiredInputConfig() const
+{
+	switch (InputMode)
+	{
+	case EGYWidgetInputMode::Game: // 게임 모드 - 조작o
+		return FUIInputConfig(ECommonInputMode::Game, GameMouseCaptureMode);
+	case EGYWidgetInputMode::Menu: // 메뉴 모드 - 조작x, UI만 조작
+		return FUIInputConfig(ECommonInputMode::Menu, EMouseCaptureMode::NoCapture);
+	case EGYWidgetInputMode::Default:
+	default:
+		return TOptional<FUIInputConfig>();
+	}
+}
