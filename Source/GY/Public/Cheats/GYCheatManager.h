aaf8dc4dd53c937eafe5f344fefcc0fda05f2535
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-05-20 14:30:58 +0900

    feat : Enemy cheat debug 추가

--- Source/GY/Public/Cheats/GYCheatManager.h
+++ Source/GY/Public/Cheats/GYCheatManager.h
@@ -3,10 +3,11 @@
 #include "CoreMinimal.h"
 #include "GameFramework/CheatManager.h"
 #include "GameplayTagContainer.h"
+#include "Enemy/Config/EnemyDataAsset.h"
 #include "GYCheatManager.generated.h"
 
 class AActor;
-
+class AGYEnemyCharacterBase;
 UCLASS()
 class GY_API UGYCheatManager : public UCheatManager
 {
@@ -46,6 +47,15 @@ public:
 	UFUNCTION(Exec)
 	void GY_TakeAllLoot();
 
+	// Enemy Debug
+	UFUNCTION(Exec)
+	void GY_SpawnEnemy(const FString& EnemyTypeName);
+
+	UFUNCTION(Exec)
+	void GY_KillAllEnemies();
+
+	UFUNCTION(Exec)
+	void GY_SetEnemyBB(const FString& KeyName, bool bValue);
 private:
 	UFUNCTION(Server, Reliable)
 	void Server_AddItem(const FString& ItemPath, int32 Count);
@@ -67,4 +77,13 @@ private:
 
 	UFUNCTION(Server, Reliable)
 	void Server_TakeAllLoot(AActor* Box);
+
+	UFUNCTION(Server, Reliable)
+	void Server_SpawnEnemy(EEnemyType EnemyType);
+
+	UFUNCTION(Server, Reliable)
+	void Server_KillAllEnemies();
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Enemy|Debug")
+	TMap<EEnemyType, TSoftClassPtr<AGYEnemyCharacterBase>> EnemyClassMap;
 };
