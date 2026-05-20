601d295e8f64281002e4df997cead90dad5ea8da
yoonseo <cysyes0403@naver.com>
2026-05-20 18:08:37 +0900

    feat: 카메라 게임플레이 태그

---
+++ Source/GY/Private/Core/GameplayTags/CameraTags.cpp
@@ -0,0 +1,10 @@
+﻿#include "Core/GameplayTags/CameraTags.h"
+
+namespace GYGameplayTags
+{
+	// Ability Type
+	UE_DEFINE_GAMEPLAY_TAG(Camera_Mode_Exploration,"Camera.Mode.Exploration"); // 확대
+	UE_DEFINE_GAMEPLAY_TAG(Camera_Mode_Boss,"Camera.Mode.Boss"); // 보스 플레이어 위치 보간
+	UE_DEFINE_GAMEPLAY_TAG(Camera_Mode_Combat,"Camera.Mode.Combat"); // 전투
+	UE_DEFINE_GAMEPLAY_TAG(Camera_Mode_Cinematic,"Camera.Mode.Cinematic"); // 시네마틱
+}
---
+++ Source/GY/Public/Core/GameplayTags/CameraTags.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GYGameplayTags
+{
+	// 카메라 제어 태그
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Camera_Mode_Exploration);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Camera_Mode_Boss);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Camera_Mode_Combat);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Camera_Mode_Cinematic);
+}
