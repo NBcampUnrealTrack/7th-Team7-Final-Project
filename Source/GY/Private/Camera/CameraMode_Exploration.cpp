8e2e66ac05e79707e92ab05606b936e6c5234ab3
yoonseo <cysyes0403@naver.com>
2026-05-21 01:59:57 +0900

    feat: 카메라 모드 - 탐구/보스

---
+++ Source/GY/Private/Camera/CameraMode_Exploration.cpp
@@ -0,0 +1,21 @@
+﻿#include "Camera/CameraMode_Exploration.h"
+#include "Camera/GYCameraComponent.h"
+#include "Camera/GYCameraModeData.h"
+
+void UCameraMode_Exploration::UpdateCamera(float DeltaTime, FGYCameraView& OutView)
+{
+	Super::UpdateCamera(DeltaTime, OutView);
+
+	APawn* Pawn = GetPawn();
+
+	if (!Pawn || !CameraData)
+	{
+		return;
+	}
+
+	// 플레이어 위치
+	FVector PlayerLocation = Pawn->GetActorLocation();
+
+	// 최종 Pivot
+	OutView.PivotLocation = PlayerLocation + CameraData->SocketOffset;
+}
