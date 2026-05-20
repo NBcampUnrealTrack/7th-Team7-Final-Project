37588dfbf3793e01ea375a150a442f1f772c6cd8
yoonseo <cysyes0403@naver.com>
2026-05-21 01:57:59 +0900

    feat: 카메라 모드 베이스

---
+++ Source/GY/Private/Camera/GYCameraModeBase.cpp
@@ -0,0 +1,62 @@
+﻿#include "Camera/GYCameraModeBase.h"
+#include "Camera/GYCameraComponent.h"
+#include "Camera/GYCameraModeData.h"
+#include "GameFramework/Pawn.h"
+
+void UGYCameraModeBase::Initialize(
+	UGYCameraComponent* InCameraComponent,
+	UGYCameraModeData* InCameraData)
+{
+	CameraComponent = InCameraComponent;
+	CameraData = InCameraData;
+}
+
+void UGYCameraModeBase::EnterMode()
+{
+}
+
+void UGYCameraModeBase::ExitMode()
+{
+}
+
+void UGYCameraModeBase::UpdateCamera(
+	float DeltaTime,
+	FGYCameraView& OutView)
+{
+	if (!CameraData)
+	{
+		return;
+	}
+
+	APawn* Pawn = GetPawn();
+
+	if (!Pawn)
+	{
+		return;
+	}
+
+	OutView.PivotLocation =
+		Pawn->GetActorLocation();
+
+	OutView.TargetArmLength = CameraData->TargetArmLength;
+
+	OutView.FOV = CameraData->FOV;
+
+	OutView.SocketOffset = CameraData->SocketOffset;
+
+	OutView.LocationInterpSpeed = CameraData->LocationInterpSpeed;
+
+	OutView.ZoomInterpSpeed = CameraData->ZoomInterpSpeed;
+
+	OutView.FOVInterpSpeed = CameraData->FOVInterpSpeed;
+}
+
+APawn* UGYCameraModeBase::GetPawn() const
+{
+	if (!CameraComponent)
+	{
+		return nullptr;
+	}
+
+	return CameraComponent->GetPawn<APawn>();
+}
