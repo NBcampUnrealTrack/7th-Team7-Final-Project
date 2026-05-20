25bf601cd9ae2cd2f9f5396116f5da6681f71a9e
KimHyeongBaek <kbrother102@gmail.com>
2026-05-20 10:02:07 +0900

    fix : 폴더 네이밍 Component -> Camera 변경

--- Source/GY/Private/Camera/GYCameraComponent.cpp
+++ Source/GY/Private/Camera/GYCameraComponent.cpp
@@ -1,7 +1,7 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Component/GYCameraComponent.h"
+#include "Camera/GYCameraComponent.h"
 
 #include "Camera/CameraComponent.h"
 #include "Character/GYPawnExtensionComponent.h"
