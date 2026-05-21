134c18b39c0e6628e91555edf6315e81b86fa8ec
dooyeon kang <dyhouse0517@gmail.com>
2026-05-21 12:56:44 +0900

    refactor: GYPlayerController의 폰 컴포넌트 결합 제거

--- Source/GY/Private/Player/GYPlayerController.cpp
+++ Source/GY/Private/Player/GYPlayerController.cpp
@@ -1,7 +1,5 @@
 #include "Player/GYPlayerController.h"
 #include "Cheats/GYCheatManager.h"
-#include "Character/GYHeroComponent.h"
-#include "Character/GYPawnExtensionComponent.h"
 
 AGYPlayerController::AGYPlayerController()
 {
@@ -11,23 +9,4 @@ AGYPlayerController::AGYPlayerController()
 void AGYPlayerController::SetupInputComponent()
 {
 	Super::SetupInputComponent();
-}
-
-void AGYPlayerController::OnRep_PlayerState()
-{
-	Super::OnRep_PlayerState();
-
-	// 이 시점에 Controller->PlayerState가 세팅됨
-	// HeroComp의 bHasControllerPairedWithPS 조건이 이제 통과 가능
-	APawn* ControlledPawn = GetPawn();
-	if (!ControlledPawn) return;
-
-	if (UGYPawnExtensionComponent* ExtComp = ControlledPawn->FindComponentByClass<UGYPawnExtensionComponent>())
-	{
-		ExtComp->CheckDefaultInitialization();
-	}
-	if (UGYHeroComponent* HeroComp = ControlledPawn->FindComponentByClass<UGYHeroComponent>())
-	{
-		HeroComp->CheckDefaultInitialization();
-	}
 }
