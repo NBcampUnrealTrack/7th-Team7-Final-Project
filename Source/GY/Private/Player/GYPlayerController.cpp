836f0f8bc05ce8c703e4f168f61bfad7410c4a31
KimHyeongBaek <kbrother102@gmail.com>
2026-05-21 11:31:48 +0900

    feat : 임시 수정본, 동작은 하게 해놓음.
    다른사람 테스트를 위해서 임시로 조치

--- Source/GY/Private/Player/GYPlayerController.cpp
+++ Source/GY/Private/Player/GYPlayerController.cpp
@@ -1,5 +1,7 @@
 #include "Player/GYPlayerController.h"
 #include "Cheats/GYCheatManager.h"
+#include "Character/GYHeroComponent.h"
+#include "Character/GYPawnExtensionComponent.h"
 
 AGYPlayerController::AGYPlayerController()
 {
@@ -9,4 +11,23 @@ AGYPlayerController::AGYPlayerController()
 void AGYPlayerController::SetupInputComponent()
 {
 	Super::SetupInputComponent();
+}
+
+void AGYPlayerController::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+
+	// 이 시점에 Controller->PlayerState가 세팅됨
+	// HeroComp의 bHasControllerPairedWithPS 조건이 이제 통과 가능
+	APawn* ControlledPawn = GetPawn();
+	if (!ControlledPawn) return;
+
+	if (UGYPawnExtensionComponent* ExtComp = ControlledPawn->FindComponentByClass<UGYPawnExtensionComponent>())
+	{
+		ExtComp->CheckDefaultInitialization();
+	}
+	if (UGYHeroComponent* HeroComp = ControlledPawn->FindComponentByClass<UGYHeroComponent>())
+	{
+		HeroComp->CheckDefaultInitialization();
+	}
 }
