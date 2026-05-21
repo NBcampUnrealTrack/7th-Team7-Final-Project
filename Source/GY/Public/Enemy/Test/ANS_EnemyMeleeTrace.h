135373430440c4330f4b6fec594a746737601111
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-05-21 09:35:01 +0900

    feat : Test용 임시 공격 Notify 구현

---
+++ Source/GY/Public/Enemy/Test/ANS_EnemyMeleeTrace.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "ANS_EnemyMeleeTrace.generated.h"
+
+UCLASS()
+class GY_API UANS_EnemyMeleeTrace : public UAnimNotifyState
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = "Trace")
+	FName StartBone = TEXT("hand_r");
+	UPROPERTY(EditAnywhere, Category = "Trace")
+	FName EndBone = TEXT("hand_r");
+	UPROPERTY(EditAnywhere, Category = "Trace")
+	float Radius = 30.f;
+	UPROPERTY(EditAnywhere, Category = "Trace")
+	float Damage = 10.f;
+	UPROPERTY(EditAnywhere, Category = "Trace")
+	bool bDebug = true;
+
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+		float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+		float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+		const FAnimNotifyEventReference& EventReference) override;
+
+private:
+	TSet<TWeakObjectPtr<AActor>> AlreadyHit;
+};
