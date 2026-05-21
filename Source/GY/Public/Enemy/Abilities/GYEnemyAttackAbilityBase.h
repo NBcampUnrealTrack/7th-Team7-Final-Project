27529df33faf7111e03d73b6ade329268e090020
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-05-21 09:36:11 +0900

    feat : Attack Base ability ActivateAbility 구현

--- Source/GY/Public/Enemy/Abilities/GYEnemyAttackAbilityBase.h
+++ Source/GY/Public/Enemy/Abilities/GYEnemyAttackAbilityBase.h
@@ -9,6 +9,7 @@ class GY_API UGYEnemyAttackAbilityBase : public UGYGameplayAbility
 {
 	GENERATED_BODY()
 public:
+	UGYEnemyAttackAbilityBase();
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat|Selection")
 	float AttackRange = 200.f;
 
@@ -18,7 +19,23 @@ public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat|Selection")
 	bool bHasCooldown = false;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat|Montage")
+	TObjectPtr<UAnimMontage> AttackMontage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat|Montage")
+	float PlayRate = 1.f;
+
 	bool CanBeSelectedByAI(const UAbilitySystemComponent* ASC, float DistToTarget) const;
 
 	float GetRemainingCooldown(const UAbilitySystemComponent* ASC) const;
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
+
+	UFUNCTION()
+	void OnMontageFinished();
+	UFUNCTION()
+	void OnMontageInterrupted();
 };
