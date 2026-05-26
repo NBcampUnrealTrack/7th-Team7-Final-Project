ad973657ab96bcd5a6e3bfb04ed985a0260fed08
yoonseo <cysyes0403@naver.com>
2026-05-26 14:23:30 +0900

    fix: 카메라 탐구 모드 타겟 추가

--- Source/GY/Private/Camera/CameraMode_Exploration.cpp
+++ Source/GY/Private/Camera/CameraMode_Exploration.cpp
@@ -1,6 +1,14 @@
 ﻿#include "Camera/CameraMode_Exploration.h"
+
+#include "EngineUtils.h"
 #include "Camera/GYCameraComponent.h"
 #include "Camera/GYCameraModeData.h"
+
+void UCameraMode_Exploration::ExitMode()
+{
+	Super::ExitMode();
+	CachedTargetActor.Reset();
+}
 
 void UCameraMode_Exploration::UpdateCamera(float DeltaTime, FGYCameraView& OutView)
 {
@@ -13,9 +21,47 @@ void UCameraMode_Exploration::UpdateCamera(float DeltaTime, FGYCameraView& OutVi
 		return;
 	}
 
+	if (!CachedTargetActor.IsValid())
+	{
+		CachedTargetActor = FindTargetActor();
+	}
+	if (!CachedTargetActor.IsValid()) return;
+
 	// 플레이어 위치
-	FVector PlayerLocation = Pawn->GetActorLocation();
+	const FVector PlayerLoc = Pawn->GetActorLocation();
+	const FVector TargetLoc = CachedTargetActor->GetActorLocation();
+	const float Distance = FVector::Dist(PlayerLoc, TargetLoc);
 
 	// 최종 Pivot
-	OutView.PivotLocation = PlayerLocation + CameraData->SocketOffset;
+	OutView.PivotLocation = FMath::Lerp(PlayerLoc, TargetLoc, CameraData->PlayerWeight);
+
+	// 거리에 따라 줌 인
+	OutView.TargetArmLength = FMath::Clamp(Distance * CameraData->ArmLengthScale,
+	                                       CameraData->MinArmLength, CameraData->MaxArmLength);
+}
+
+AActor* UCameraMode_Exploration::FindTargetActor() const
+{
+	if (!CameraComponent)
+	{
+		return nullptr;
+	}
+
+	UWorld* World = CameraComponent->GetWorld();
+	if (!World)
+	{
+		return nullptr;
+	}
+
+	// 월드의 모든 Actor 중 TargetActorTag를 가진 액터를 찾음
+	for (TActorIterator<AActor> It(World); It; ++It)
+	{
+		AActor* Actor = *It;
+		if (Actor->ActorHasTag(TargetActorTag))
+		{
+			return Actor;
+		}
+	}
+
+	return nullptr;
 }
--- Source/GY/Public/Camera/CameraMode_Exploration.h
+++ Source/GY/Public/Camera/CameraMode_Exploration.h
@@ -10,7 +10,16 @@ class GY_API UCameraMode_Exploration : public UGYCameraModeBase
 	GENERATED_BODY()
 
 public:
+	virtual void ExitMode() override;
 	virtual void UpdateCamera(
 		float DeltaTime,
 		FGYCameraView& OutView) override;
+
+private:
+	AActor* FindTargetActor() const;
+
+	TWeakObjectPtr<AActor> CachedTargetActor;
+
+	UPROPERTY(EditDefaultsOnly, Category="Camera")
+	FName TargetActorTag = FName("Target");
 };
