ac94581555b4d0b0195f678a466aed676db13db1
KimHyeongBaek <kbrother102@gmail.com>
2026-05-27 12:44:11 +0900

    feat : 하위 레이어 생성 및 부모클래스 변경

--- Source/GY/GY.Build.cs
+++ Source/GY/GY.Build.cs
@@ -26,6 +26,6 @@ public class GY : ModuleRules
 			"GameFeatures",
 		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] { });
+		PrivateDependencyModuleNames.AddRange(new string[] { "AnimGraphRuntime" });
 	}
 }
