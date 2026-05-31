067ab8f1e502233ef057f95e7fea3ab12ef2eefb
KimHyeongBaek <kbrother102@gmail.com>
2026-06-01 04:49:42 +0900

    feat : 탈진 태그 추가

--- Source/GY/Private/Core/GameplayTags/StateTags.cpp
+++ Source/GY/Private/Core/GameplayTags/StateTags.cpp
@@ -11,6 +11,9 @@ namespace GYStateTags
 	UE_DEFINE_GAMEPLAY_TAG(State_Hit_KnockDown, "State.Hit.KnockDown");
 	UE_DEFINE_GAMEPLAY_TAG(State_Hit_Stun, "State.Hit.Stun");
 
+	UE_DEFINE_GAMEPLAY_TAG(State_Exhausted, "State.Exhausted");
+
+
 	UE_DEFINE_GAMEPLAY_TAG(State_Combat_InCombat, "State.Combat.InCombat");
 	UE_DEFINE_GAMEPLAY_TAG(State_Combat_SuperArmor, "State.Combat.SuperArmor");
 
