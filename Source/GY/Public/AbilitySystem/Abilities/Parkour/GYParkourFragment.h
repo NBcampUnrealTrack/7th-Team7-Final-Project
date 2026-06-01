0a8338deb77812a81e9a8dc81022077e5cfa8f01
KimHyeongBaek <kbrother102@gmail.com>
2026-06-01 20:45:42 +0900

    feat : 어빌리티 프레그먼트, 로직 클래스 제작

---
+++ Source/GY/Public/AbilitySystem/Abilities/Parkour/GYParkourFragment.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/Fragment/AbilityFragment.h"
+#include "GYParkourFragment.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GY_API UGYParkourFragment : public UAbilityFragment
+{
+	GENERATED_BODY()
+};
---
+++ Source/GY/Public/AbilitySystem/Abilities/Parkour/GYParkourLogic.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/Logic/AbilityLogicBase.h"
+#include "GYParkourLogic.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GY_API UGYParkourLogic : public UAbilityLogicBase
+{
+	GENERATED_BODY()
+};
