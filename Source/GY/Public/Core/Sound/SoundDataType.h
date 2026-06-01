d241ad172c852792f081ba21a3b05c13442d2b50
yoonseo <cysyes0403@naver.com>
2026-06-01 09:57:58 +0900

    feat: 사운드 데이터

---
+++ Source/GY/Public/Core/Sound/SoundDataType.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "UObject/Object.h"
+#include "SoundDataType.generated.h"
+
+UENUM(BlueprintType)
+enum class EGYSoundCategory : uint8
+{
+	BGM,
+	SFX,
+	UI,
+	Voice,
+	Ambient,
+	Master
+};
+
+USTRUCT(BlueprintType)
+struct GY_API FGYSoundDataTableRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	FGameplayTag Tag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	EGYSoundCategory Category = EGYSoundCategory::BGM;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	TObjectPtr<USoundBase> Sound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	float Volume = 1.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	float Pitch = 1.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	float FadeInTime = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	float FadeOutTime = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	float StartTime = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	TObjectPtr<USoundAttenuation> Attenuation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	TObjectPtr<USoundConcurrency> Concurrency;
+};
