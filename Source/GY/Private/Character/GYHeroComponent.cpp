7c69368545a5aecf0d5d99fe51a6e838d7401fd6
KHB <kbrother102@gmail.com>
2026-06-04 20:59:57 +0900

    fix : 락온후 움직임 픽스 (#110)

--- Source/GY/Private/Character/GYHeroComponent.cpp
+++ Source/GY/Private/Character/GYHeroComponent.cpp
@@ -237,23 +237,25 @@ void UGYHeroComponent::Input_Move(const FInputActionValue& InputActionValue)
 	APawn* Pawn = GetPawn<APawn>();
 	AController* Controller = Pawn ? Pawn->GetController() : nullptr;
 
-	if (Controller)
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+	if (PlayerController->PlayerCameraManager && PlayerController)
 	{
 		const FVector2D Value = InputActionValue.Get<FVector2D>();
-		const FRotator MovementRotation(0.0f, Controller->GetControlRotation().Yaw, 0.0f);
+		const FRotator CameraRotation(0.0f, PlayerController->PlayerCameraManager->GetCameraRotation().Yaw, 0.0f);
 
 		if (Value.X != 0.0f)
 		{
-			const FVector MovementDirection = MovementRotation.RotateVector(FVector::RightVector);
+			const FVector MovementDirection = CameraRotation.RotateVector(FVector::RightVector);
 			Pawn->AddMovementInput(MovementDirection, Value.X);
 		}
 
 		if (Value.Y != 0.0f)
 		{
-			const FVector MovementDirection = MovementRotation.RotateVector(FVector::ForwardVector);
+			const FVector MovementDirection = CameraRotation.RotateVector(FVector::ForwardVector);
 			Pawn->AddMovementInput(MovementDirection, Value.Y);
 		}
 	}
+
 }
 
 void UGYHeroComponent::Input_AbilityInputTagPressed(FGameplayTag InputTag)
