e1fb4e78473e44c5dcb11b3626687b96955c16fc
yoonseo <cysyes0403@naver.com>
2026-06-04 20:46:19 +0900

    feat: 사운드 태그

---
+++ Source/GY/Public/Core/GameplayTags/SoundTags.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GYGameplayTags
+{
+	/* 플레이어 사운드 */
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Walk)
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Run)
+
+	/* 적 사운드 */
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Goblin_Roar)
+
+	/* BGM */
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_BGM_Combat)
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_BGM_Boss)
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_BGM_Lobby)
+
+	/* Voice */
+}
