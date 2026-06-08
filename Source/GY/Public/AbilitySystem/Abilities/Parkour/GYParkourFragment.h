43ae4b4afef22ee7dd1fda21efe199c37642a69d
KimHyeongBaek <kbrother102@gmail.com>
2026-06-08 14:56:22 +0900

    feat : 파쿠르 어빌리티 로직과 프레그먼트 제작

--- Source/GY/Public/AbilitySystem/Abilities/Parkour/GYParkourFragment.h
+++ Source/GY/Public/AbilitySystem/Abilities/Parkour/GYParkourFragment.h
@@ -4,13 +4,42 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystem/Abilities/Fragment/AbilityFragment.h"
+#include "Core/GameplayTags/AbilityTags.h"
 #include "GYParkourFragment.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class GY_API UGYParkourFragment : public UAbilityFragment
 {
 	GENERATED_BODY()
+public:
+
+	UGYParkourFragment() { FragmentTag = GYGameplayTags::Ability_Fragment_Parkour; };
+
+	UPROPERTY(EditDefaultsOnly)
+	UAnimMontage* Montage_Run_Lfoot;
+	UPROPERTY(EditDefaultsOnly)
+	UAnimMontage* Montage_Run_Rfoot;
+	UPROPERTY(EditDefaultsOnly)
+	UAnimMontage* Montage_Stand_Lfoot;
+	UPROPERTY(EditDefaultsOnly)
+	UAnimMontage* Montage_Stand_Rfoot;
+	UPROPERTY(EditDefaultsOnly)
+	UAnimMontage* Montage_Walk_Lfoot;
+	UPROPERTY(EditDefaultsOnly)
+	UAnimMontage* Montage_Walk_Rfoot;
+
+	// 트레이스 파라미터
+	UPROPERTY(EditDefaultsOnly)
+	float ForwardTraceDistance = 75.f;   // 전방 벽 감지 거리
+	UPROPERTY(EditDefaultsOnly)
+	float TraceHeightOffset    = 500.f;  // 위에서 아래로 트레이스 시작 높이 - 충분히 높게 잡을 것
+	UPROPERTY(EditDefaultsOnly)
+	float LowMantleMaxHeight   = 80.f;   // run 몽타주 상한 높이 (cm)
+	UPROPERTY(EditDefaultsOnly)
+	float MidMantleMaxHeight   = 150.f;  // stand 몽타주 상한 높이 (cm)
+	// MidMantleMaxHeight 초과 → walk 몽타주
+
 };
