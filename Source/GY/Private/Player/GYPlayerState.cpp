6b749abe3f14746e3e4501c45ab09e9336cdd57c
Bill Lee <ood11611doo@gmail.com>
2026-06-08 19:21:50 +0900

    feat: KAN-253, 플레이어 사망 및 부활 관리 작업 (#130)
    * feat: KAN-253, 플레이어 사망 및 부활 관리 작업
    
    * fix: Standalone에서 콤보 관련 오류 픽스
    
    * Revert "fix: Standalone에서 콤보 관련 오류 픽스"
    
    This reverts commit 80f527d6b65777e269e97c062ffa5cec75b48a29.

--- Source/GY/Private/Player/GYPlayerState.cpp
+++ Source/GY/Private/Player/GYPlayerState.cpp
@@ -55,6 +55,8 @@ void AGYPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLi
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 	DOREPLIFETIME(AGYPlayerState, PawnData);
+	DOREPLIFETIME(AGYPlayerState, LastCheckpointLocation);
+	DOREPLIFETIME(AGYPlayerState, bHasCheckpoint);
 }
 
 void AGYPlayerState::SetPawnData(const UGYPawnData* InPawnData)
@@ -79,6 +81,9 @@ void AGYPlayerState::InitGAS(APawn* Avatar)
 	AbilitySystemComponent->InitAbilityActorInfo(this, Avatar);
 
 	if (GetLocalRole() != ROLE_Authority) return;
+	if (bAttributesInitialized) return;
+	bAttributesInitialized = true;
+	InitialSpawnLocation = Avatar->GetActorLocation();
 
 	AbilitySystemComponent->AddLooseGameplayTag(
 		GYFactionTags::Character_Faction_Player, 1,
@@ -101,5 +106,11 @@ void AGYPlayerState::InitGAS(APawn* Avatar)
 		AbilitySystemComponent->SetNumericAttributeBase(UGYAdditionalAttribute::GetMaxStunAttribute(),        InitData->MaxStun);
 		AbilitySystemComponent->SetNumericAttributeBase(UGYAdditionalAttribute::GetCurrentStunAttribute(),    InitData->MaxStun);
 	}
+}
+
+void AGYPlayerState::Server_SetCheckpoint_Implementation(FVector Location)
+{
+	LastCheckpointLocation = Location;
+	bHasCheckpoint = true;
 }
 
