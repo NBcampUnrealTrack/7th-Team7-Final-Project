3a8833316b7fff295137453a4d2c1193dd92063f
yoonseo <cysyes0403@naver.com>
2026-06-09 16:33:37 +0900

    fix: 사운드 태그

--- Source/GY/Private/Core/GameplayTags/SoundTags.cpp
+++ Source/GY/Private/Core/GameplayTags/SoundTags.cpp
@@ -9,6 +9,15 @@ namespace GYGameplayTags
 	/* 적 사운드 */
 	UE_DEFINE_GAMEPLAY_TAG(Sound_Goblin_Roar, "Sound.Goblin.Roar")
 
+
+	/* 상호 작용 */
+	UE_DEFINE_GAMEPLAY_TAG(Sound_Interaction_LootBox_Open, "Sound.Interaction.LootBox.Open")
+	UE_DEFINE_GAMEPLAY_TAG(Sound_Interaction_LootBox_Close, "Sound.Interaction.LootBox.Close")
+	UE_DEFINE_GAMEPLAY_TAG(Sound_Interaction_LootBox_First, "Sound.Interaction.LootBox.First")
+	UE_DEFINE_GAMEPLAY_TAG(Sound_Interaction_Door_Open, "Sound.Interaction.Door.Open")
+	UE_DEFINE_GAMEPLAY_TAG(Sound_Interaction_Door_Close, "Sound.Interaction.Door.Close")
+	UE_DEFINE_GAMEPLAY_TAG(Sound_Interaction_TimeRift, "Sound.Interaction.TimeRift")
+
 	/* BGM */
 	UE_DEFINE_GAMEPLAY_TAG(Sound_BGM_Combat, "Sound.BGM.Combat")
 	UE_DEFINE_GAMEPLAY_TAG(Sound_BGM_Boss, "Sound.BGM.Boss")
