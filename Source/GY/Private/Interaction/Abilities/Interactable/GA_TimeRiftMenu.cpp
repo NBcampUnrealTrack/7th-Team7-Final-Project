860b8e3a4c1c6ba2e35e19ae6fa5bec689df399b
yoonseo <cysyes0403@naver.com>
2026-06-10 12:44:55 +0900

    feat: 시간의 틈 SFX, 카메라
    ^ Conflicts:
    ^	Source/GY/Private/Interaction/Abilities/Interactable/GA_TimeRiftMenu.cpp

--- Source/GY/Private/Interaction/Abilities/Interactable/GA_TimeRiftMenu.cpp
+++ Source/GY/Private/Interaction/Abilities/Interactable/GA_TimeRiftMenu.cpp
@@ -2,11 +2,15 @@
 
 #include "AbilitySystemComponent.h"
 #include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+#include "Core/GameplayTags/CameraTags.h"
 #include "Core/GameplayTags/EventTags.h"
+#include "Core/GameplayTags/SoundTags.h"
 #include "Core/GameplayTags/StateTags.h"
 #include "Player/GYPlayerState.h"
 #include "WorldGimmick/TimeRift/TimeRift.h"
 #include "WorldGimmick/TimeRift/TimeRiftSubsystem.h"
+#include "Core/Sound/GYSoundManager.h"
+#include "Logging/GYLogManager.h"
 
 UGA_TimeRiftMenu::UGA_TimeRiftMenu(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -17,13 +21,15 @@ UGA_TimeRiftMenu::UGA_TimeRiftMenu(const FObjectInitializer& ObjectInitializer)
 }
 
 void UGA_TimeRiftMenu::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
-	const FGameplayEventData* TriggerEventData)
+                                       const FGameplayAbilityActorInfo* ActorInfo,
+                                       const FGameplayAbilityActivationInfo ActivationInfo,
+                                       const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
 
-	UAbilityTask_WaitGameplayEvent* Task =  UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(this, GYGameplayTags::Event_TimeRift_Exit);
+	UAbilityTask_WaitGameplayEvent* Task = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+		this, GYGameplayTags::Event_TimeRift_Exit);
 	Task->EventReceived.AddDynamic(this, &ThisClass::OnExitEventReceived);
 	Task->ReadyForActivation();
 
@@ -47,9 +53,33 @@ void UGA_TimeRiftMenu::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 		}
 
 	}
+	GY_LOG(Content, CYS, "시간틈 메뉴열기");
+	// 카메라 모드 태그
+	AGYPlayerState* PS = Cast<AGYPlayerState>(ActorInfo->OwnerActor.Get());
+	if (!PS) return;
+
+	UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+	if (!ASC) return;
+
+	ASC->AddLooseGameplayTag(GYGameplayTags::Camera_Mode_ZoomIn, 1, EGameplayTagReplicationState::CountToOwner);
+
+	if (UGYSoundManager* SoundManager = UGYSoundManager::Get(this))
+	{
+		// 사운드
+		SoundManager->PlaySound2D(GYGameplayTags::Sound_Interaction_TimeRift);
+	}
 }
 
 void UGA_TimeRiftMenu::OnExitEventReceived(FGameplayEventData Payload)
 {
 	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+	GY_LOG(Content, CYS, "시간틈 메뉴끝");
+	// 카메라 모드 태그 제거
+	AGYPlayerState* PS = Cast<AGYPlayerState>(CurrentActorInfo->OwnerActor.Get());
+	if (!PS) return;
+
+	UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+	if (!ASC) return;
+
+	ASC->RemoveLooseGameplayTag(GYGameplayTags::Camera_Mode_ZoomIn, 1, EGameplayTagReplicationState::CountToOwner);
 }
