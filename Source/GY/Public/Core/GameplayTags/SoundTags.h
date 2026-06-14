46444e69052d507e56d1b497dd6e5ab4c1933d0a
yoonseo <cysyes0403@naver.com>
2026-06-15 00:12:31 +0900

    feat: 플레이어 기본 액션 SFX

--- Source/GY/Public/Core/GameplayTags/SoundTags.h
+++ Source/GY/Public/Core/GameplayTags/SoundTags.h
@@ -8,6 +8,28 @@ namespace GYGameplayTags
 	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Walk)
 	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Run)
 
+	// 약공격
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Attack_Light);
+
+	// 강공격
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Attack_Heavy_Charge);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Attack_Heavy_Release);
+
+	// 패링
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Parry_Attempt);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Parry_Success);
+
+	// 방패 블로킹
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Block_Success);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Block_Break);
+
+	// 회피
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Dodge);
+
+	// 물약
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Potion);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Player_Healing);
+
 	/* 적 사운드 */
 	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sound_Goblin_Roar)
 
