191d789e5754e23aa177a57d993eb8cfa12e90ba
KimHyeongBaek <kbrother102@gmail.com>
2026-06-15 19:33:45 +0900

    feat : IsEmpty로 바꿧기 때문에 NotBool 추가

--- Source/GY/Private/AttackLogic/Dodge/GYDodgeMontageFragment.cpp
+++ Source/GY/Private/AttackLogic/Dodge/GYDodgeMontageFragment.cpp
@@ -14,13 +14,13 @@ const FGYDodgeMontageSet* UGYDodgeMontageFragment::GetBestMatchingSet(const FGam
 	{
 		if (!Pair.Key.IsValid())
 		{
-			if (Pair.Value.DirectionalMontages.IsEmpty())
+			if (!Pair.Value.DirectionalMontages.IsEmpty())
 				DefaultResult = &Pair.Value;
 			continue;
 		}
 		if (OwnedTags.HasTag(Pair.Key))
 		{
-			return Pair.Value.DirectionalMontages.IsEmpty() ? &Pair.Value : nullptr;
+			return !Pair.Value.DirectionalMontages.IsEmpty() ? &Pair.Value : nullptr;
 		}
 	}
 
