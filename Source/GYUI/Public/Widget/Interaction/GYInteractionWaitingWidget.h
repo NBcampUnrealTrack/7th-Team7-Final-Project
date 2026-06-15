e4b95b1531185a24249d0c175dcd1b7a3e35dcf6
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-06-15 20:42:58 +0900

    feat : 상호작용 대기 위젯 구현

---
+++ Source/GYUI/Public/Widget/Interaction/GYInteractionWaitingWidget.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/GYActivatableWidget.h"
+#include "GameFramework/GameplayMessageSubsystem.h"
+#include "GYInteractionWaitingWidget.generated.h"
+
+class UTextBlock;
+struct FGYInteractionWaitingMessage;
+
+/**
+ * 상호작용 후 플레이어 대기 위젯
+ */
+UCLASS(Abstract, Blueprintable)
+class GYUI_API UGYInteractionWaitingWidget : public UGYActivatableWidget
+{
+	GENERATED_BODY()
+
+public:
+	UGYInteractionWaitingWidget(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintCallable, Category = "GY|Ending")
+	void SetCount(int32 Current, int32 Required);
+
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> CountText;
+	UPROPERTY(EditDefaultsOnly, Category = "Waiting")
+	FText CountFormat;
+	UFUNCTION(BlueprintImplementableEvent, Category = "Waiting")
+	void OnCountUpdated(int32 Current, int32 Required);
+
+private:
+	void HandleWaitingMessage(FGameplayTag, const FGYInteractionWaitingMessage& Msg);
+
+	FGameplayMessageListenerHandle ListenerHandle;
+};
