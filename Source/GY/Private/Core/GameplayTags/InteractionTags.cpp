82ad6feaee7feca0dcf3be7d29b204bf606d4ba2
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-06-15 20:31:20 +0900

    feat : 엔딩(=시네마틱) 시작 채크 추가

--- Source/GY/Private/Core/GameplayTags/InteractionTags.cpp
+++ Source/GY/Private/Core/GameplayTags/InteractionTags.cpp
@@ -6,5 +6,5 @@ namespace GYGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(Interaction_Open_Door, "Interaction.Open.Door")
 	UE_DEFINE_GAMEPLAY_TAG(Interaction_TimeRift_Sit, "Interaction.TimeRift.Sit")
 	UE_DEFINE_GAMEPLAY_TAG(Interaction_Ladder_Activate, "Interaction.Ladder.Activate")
-
+	UE_DEFINE_GAMEPLAY_TAG(Interaction_Ending_Start, "Interaction.Ending.Start");
 }
