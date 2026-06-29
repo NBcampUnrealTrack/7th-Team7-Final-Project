fb2295a564db61f8bb765390d2634c81b26f65c3
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-06-29 21:47:13 +0900

    fix: 로그 삭제

--- Source/GY/Private/Enemy/Component/EnemyAggroComponent.cpp
+++ Source/GY/Private/Enemy/Component/EnemyAggroComponent.cpp
@@ -148,12 +148,12 @@ void UEnemyAggroComponent::UnbindFromPerception()
 
 void UEnemyAggroComponent::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	UE_LOG(LogTemp, Warning,
-		TEXT("[EnemyAggro] PerceptionUpdated Actor=%s Type=%s Sensed=%d Strength=%.1f"),
-		*GetNameSafe(Actor),
-		*Stimulus.Type.Name.ToString(),
-		Stimulus.WasSuccessfullySensed() ? 1 : 0,
-		Stimulus.Strength);
+	// UE_LOG(LogTemp, Warning,
+	// 	TEXT("[EnemyAggro] PerceptionUpdated Actor=%s Type=%s Sensed=%d Strength=%.1f"),
+	// 	*GetNameSafe(Actor),
+	// 	*Stimulus.Type.Name.ToString(),
+	// 	Stimulus.WasSuccessfullySensed() ? 1 : 0,
+	// 	Stimulus.Strength);
 	if (!Actor || !GetOwner() || !GetOwner()->HasAuthority()) return;
 
 	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Actor);
