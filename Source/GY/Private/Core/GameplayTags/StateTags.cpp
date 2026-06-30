cdaec0538283a2954674b88040f77b0e198b7eb6
KimHyeongBaek <kbrother102@gmail.com>
2026-06-30 17:21:57 +0900

    feat : 락온 시작과 끝에 State_Lockon 태그 추가

--- Source/GY/Private/Core/GameplayTags/StateTags.cpp
+++ Source/GY/Private/Core/GameplayTags/StateTags.cpp
@@ -20,6 +20,8 @@ namespace GYStateTags
 	UE_DEFINE_GAMEPLAY_TAG(State_Combat_SuperArmor, "State.Combat.SuperArmor");
 	UE_DEFINE_GAMEPLAY_TAG(State_Combat_Invulnerable, "State.Combat.Invulnerable");
 
+	UE_DEFINE_GAMEPLAY_TAG(State_LockOn, "State.LockOn")
+
 	UE_DEFINE_GAMEPLAY_TAG(State_Regen_Delay_Stamina, "State.Regen.Delay.Stamina");
 	UE_DEFINE_GAMEPLAY_TAG(State_Regen_Delay_Stagger, "State.Regen.Delay.Stagger");
 	UE_DEFINE_GAMEPLAY_TAG(State_Regen_Delay_Stun, "State.Regen.Delay.Stun");
