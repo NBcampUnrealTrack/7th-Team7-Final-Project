84b601c7dd2e561cadca5787b4ede562c2694b2c
yoonseo <cysyes0403@naver.com>
2026-07-01 14:52:16 +0900

    feat: 보스 페이즈2 카메라 모드

--- Source/GY/Private/Core/GameplayTags/CameraTags.cpp
+++ Source/GY/Private/Core/GameplayTags/CameraTags.cpp
@@ -5,6 +5,7 @@ namespace GYGameplayTags
 	// 카메라 모드
 	UE_DEFINE_GAMEPLAY_TAG(Camera_Mode_Exploration, "Camera.Mode.Exploration"); // 확대
 	UE_DEFINE_GAMEPLAY_TAG(Camera_Mode_Boss, "Camera.Mode.Boss"); // 보스 플레이어 위치 보간
+	UE_DEFINE_GAMEPLAY_TAG(Camera_Mode_Boss_Phase2, "Camera.Mode.Boss.Phase2");
 	UE_DEFINE_GAMEPLAY_TAG(Camera_Mode_Combat, "Camera.Mode.Combat"); // 전투
 	UE_DEFINE_GAMEPLAY_TAG(Camera_Mode_Cinematic, "Camera.Mode.Cinematic"); // 시네마틱
 	UE_DEFINE_GAMEPLAY_TAG(Camera_Mode_ZoomIn, "Camera.Mode.ZoomIn");
--- Source/GY/Public/Core/GameplayTags/CameraTags.h
+++ Source/GY/Public/Core/GameplayTags/CameraTags.h
@@ -7,6 +7,7 @@ namespace GYGameplayTags
 	// 카메라 제어 태그
 	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Camera_Mode_Exploration);
 	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Camera_Mode_Boss);
+	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Camera_Mode_Boss_Phase2);
 	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Camera_Mode_Combat);
 	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Camera_Mode_Cinematic);
 	GY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Camera_Mode_ZoomIn);
