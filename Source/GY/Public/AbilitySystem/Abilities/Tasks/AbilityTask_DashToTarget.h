c5fa63f691bc72227cf01f4207bea28e42dc6e38
jcmeve <jcmeve@naver.com>
2026-07-06 02:57:25 +0900

    feat: LongSwordMontage,Dash

---
+++ Source/GY/Public/AbilitySystem/Abilities/Tasks/AbilityTask_DashToTarget.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_DashToTarget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class GY_API UAbilityTask_DashToTarget : public UAbilityTask
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category="Ability|Task",
+		meta=(HidePin="OwningAbility", DefaultToSelf="OwningAbility", BlueprintInternalUseOnly="true"))
+	static UAbilityTask_DashToTarget* CreateDashToTarget(
+		UGameplayAbility* OwningAbility,
+		AActor* Target,
+		float DashSpeed,
+		float StopDistance,
+		float FrontHalfAngleDeg = 45.f);
+
+	virtual void Activate() override;
+	virtual void TickTask(float DeltaTime) override;
+	virtual void OnDestroy(bool bInOwnerFinished) override;
+
+	UPROPERTY(BlueprintAssignable)
+	FGenericGameplayTaskDelegate OnReachedDistance;
+
+	UPROPERTY(BlueprintAssignable)
+	FGenericGameplayTaskDelegate OnOutOfSight;
+
+	UPROPERTY(BlueprintAssignable)
+	FGenericGameplayTaskDelegate OnCancelled;
+
+protected:
+	TWeakObjectPtr<AActor> TargetActor;
+
+	float DashSpeed = 800.f;
+	float StopDistanceSq = 0.f;
+	float CosFrontHalfAngle = 0.f;
+
+	bool bBroadcasted = false;
+};
