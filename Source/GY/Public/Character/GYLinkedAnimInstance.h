a1d9599850db24021aadad02ae154cc600ab5497
KimHyeongBaek <kbrother102@gmail.com>
2026-07-06 23:17:51 +0900

    feat : 레이어에 스레드세이프 함수로 연산

--- Source/GY/Public/Character/GYLinkedAnimInstance.h
+++ Source/GY/Public/Character/GYLinkedAnimInstance.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GYAnimTypes.h"
 #include "Animation/AnimInstance.h"
 #include "GYLinkedAnimInstance.generated.h"
 
@@ -10,6 +11,7 @@ class UGYCharacterAnimInstance;
 /**
  *
  */
+
 UCLASS()
 class GY_API UGYLinkedAnimInstance : public UAnimInstance
 {
@@ -17,7 +19,8 @@ class GY_API UGYLinkedAnimInstance : public UAnimInstance
 
 public:
 	virtual void NativeInitializeAnimation() override;
-
+	// 스레드 세이프 업데이트 오버라이드
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
 
 	UFUNCTION(BlueprintPure, Category = "Animation", meta = (BlueprintThreadSafe))
 	UGYCharacterAnimInstance* GetMainAnimBPThreadSafe() const;
@@ -26,6 +29,20 @@ protected:
 
 	UPROPERTY(Transient)
 	UGYCharacterAnimInstance* MainAnimInstance;
+
+
+
+	//무기별 스탑 시퀀수
+	UPROPERTY(EditDefaultsOnly, Category = "DistanceMatching")
+	FGYDirectionalStopAnims WalkStopAnimSet;
+
+	UPROPERTY(EditDefaultsOnly, Category = "DistanceMatching")
+	FGYDirectionalStopAnims RunStopAnimSet;
+
+	// 레이어 애님 그래프에 바인딩할 최종 시퀀스
+	UPROPERTY(BlueprintReadOnly, Transient, Category = "DistanceMatching|Output", meta=(BlueprintThreadSafe))
+	UAnimSequence* TargetStopSequence = nullptr;
+
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Locomotion", meta=(BlueprintThreadSafe))
 	UBlendSpace* MoveBlendSpace;
