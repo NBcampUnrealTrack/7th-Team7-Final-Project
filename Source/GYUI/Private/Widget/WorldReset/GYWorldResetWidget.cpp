06e8dab5d0f20b14678509c0cfdd0d196c87b250
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-07-11 02:11:08 +0900

    fix : 레이아웃 UX 개선 - 월드리셋 수정

--- Source/GYUI/Private/Widget/WorldReset/GYWorldResetWidget.cpp
+++ Source/GYUI/Private/Widget/WorldReset/GYWorldResetWidget.cpp
@@ -14,6 +14,11 @@ void UGYWorldResetWidget::NativeConstruct()
 	ResetHands();
 }
 
+bool UGYWorldResetWidget::NativeOnHandleBackAction()
+{
+	return true;
+}
+
 void UGYWorldResetWidget::PlayResetSequence(float HoldOverride)
 {
 	HoldDuration = HoldOverride;
