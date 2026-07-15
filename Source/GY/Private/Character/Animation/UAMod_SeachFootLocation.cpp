342e5359973325012a47b698d302891ef9b94c6e
KimHyeongBaek <kbrother102@gmail.com>
2026-07-16 06:25:23 +0900

    feat : 발자국 소리 노티파이, 마커 모디파이어

---
+++ Source/GY/Private/Character/Animation/UAMod_SeachFootLocation.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Animation/UAMod_SeachFootLocation.h"
+
+void UUAMod_SeachFootLocation::OnApply_Implementation(UAnimSequence* AnimationSequence)
+{
+	Super::OnApply_Implementation(AnimationSequence);
+	
+}
+
+void UUAMod_SeachFootLocation::OnRevert_Implementation(UAnimSequence* AnimationSequence)
+{
+	Super::OnRevert_Implementation(AnimationSequence);
+}
---
+++ Source/GY/Public/Character/Animation/UAMod_SeachFootLocation.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Editor/AnimationModifiers/Public/AnimationModifier.h"
+#include "UAMod_SeachFootLocation.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class GY_API UUAMod_SeachFootLocation : public UAnimationModifier
+{
+	GENERATED_BODY()
+public:
+	virtual void OnApply_Implementation(UAnimSequence* AnimationSequence) override;
+	virtual void OnRevert_Implementation(UAnimSequence* AnimationSequence) override;
+	protected:
+
+	//노티파이 넣을 곳 이름
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	FName NotifyTrackName = TEXT("SFX");
+
+	//싱크마커 넣을 곳 이름
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	FName SyncTrackName = TEXT("Sync");
+
+	//추가할 노티파이 클래스
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	TSubclassOf<UAnimNotify> FootstepSoundNotifyClass;
+};
