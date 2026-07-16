5162d0f6ba2f5ed53e0fbefe934f43137c38c826
jcmeve <jcmeve@naver.com>
2026-07-16 16:54:35 +0900

    fix: Hitstop, AddDynamic (#441)

--- Source/GY/Public/Enemy/Actor/GYWeaponHitBox.h
+++ Source/GY/Public/Enemy/Actor/GYWeaponHitBox.h
@@ -21,6 +21,7 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 	UFUNCTION()
 	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other,
