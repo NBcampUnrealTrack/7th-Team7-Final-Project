37cd131bf7fedcd8d2d426b1b8f005548117df20
jcmeve <jcmeve@naver.com>
2026-07-17 14:14:09 +0900

    fix: region-tag

--- Source/GY/Private/Core/GameplayTags/RegionTags.cpp
+++ Source/GY/Private/Core/GameplayTags/RegionTags.cpp
@@ -2,6 +2,8 @@
 
 namespace GYGameplayTags
 {
+	UE_DEFINE_GAMEPLAY_TAG(Region, "Region")
+
 	UE_DEFINE_GAMEPLAY_TAG(Region_Test, "Region.Test")
 	UE_DEFINE_GAMEPLAY_TAG(Region_Town, "Region.Town")
 	UE_DEFINE_GAMEPLAY_TAG(Region_Forest, "Region.Forest")
