7bcdc92a57449555a2df116ef0dafcf4d151ac6e
KHB <kbrother102@gmail.com>
2026-07-19 19:59:28 +0900

    fix :재생중인 큐 로그 (#498)

--- Source/GY/Public/Core/GameplayCue/GYGameplayCueNotify_FX.h
+++ Source/GY/Public/Core/GameplayCue/GYGameplayCueNotify_FX.h
@@ -16,6 +16,9 @@ public:
 
 	virtual bool OnActive_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters) const override;
 
+	// [진단용] 나이아가라 이펙트 누수 확인을 위해 임시로 추가 - 확인 끝나면 제거
+	virtual bool OnRemove_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters) const override;
+
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "FXCue|Animation")
 	TObjectPtr<UAnimMontage> Montage;
